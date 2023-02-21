b631bc826d34d7e47d22ecd380b86261d2cc8ab7
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2023-02-21 17:50:36 +0200

    add button story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,19 +4,23 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import './Button.module.css';
 
 export default {
-  title: 'Button',
+  title: 'Phonebook/Button',
   component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    color: { control: 'color' },
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const ButtonSubmit = Template.bind({});
-export const ButtonSubmitBig = Template.bind({});
 
 ButtonSubmit.args = {
-  children: 'add contact',
-};
-
-ButtonSubmitBig.args = {
-  children: 'press me',
+  label: 'add contact',
+  size: 'small',
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,32 @@
 import React, { FC, ReactNode } from 'react';
 import css from './Button.module.css';
+import '../../index.css';
 
 interface ButtonProps {
-  children: ReactNode;
+  primary?: boolean;
+
+  backgroundColor?: string;
+  color?: string;
+
+  size?: 'small' | 'medium' | 'large';
+
+  label: string;
+  onClick?: () => void;
 }
 
-export const Button: FC<ButtonProps> = ({ children }) => {
+export const Button: FC<ButtonProps> = ({
+  backgroundColor,
+  color,
+  label,
+  size = 'medium',
+}) => {
   return (
-    <button className={css.button} type="submit">
-      {children}
+    <button
+      className={`${css.button} ${size}`}
+      type="submit"
+      style={{ backgroundColor, color }}
+    >
+      {label}
     </button>
   );
 };
